6f0f4ba4fcb0585b145c09f08043b93456c82b4a
Pham Duc Binh <binh1298@gmail.com>
2024-05-21 22:48:21 +0700

    init config for arm macos

--- home/mac.nix
+++ home/mac.nix
@@ -1,8 +1,6 @@
 {pkgs, ...}: {
   home.stateVersion = "22.11";
   home.packages = with pkgs; [
-    ripgrep
-    fd
     curl
     less
   ];
@@ -14,20 +12,6 @@
 
   programs.bat.enable = true;
   programs.bat.config.theme = "TwoDark";
-  programs.fzf.enable = true;
-  programs.fzf.enableZshIntegration = true;
-  programs.eza.enable = true;
-  programs.git.enable = true;
-  programs.zsh.enable = true;
-  programs.zsh.enableCompletion = true;
-  programs.zsh.autosuggestion.enable = true;
-  programs.zsh.syntaxHighlighting.enable = true;
-  programs.zsh.shellAliases = {
-    ls = "ls --color=auto -F";
-  };
-  programs.starship.enable = true;
-  programs.starship.enableZshIntegration = true;
-
   home.file.".inputrc".source = ./dotfiles/inputrc;
 
   xdg.configFile."nvim/" = {
@@ -40,5 +24,7 @@
     ./shared/dev
     ./shared/cli-apps
     ./shared/desktop
+    ./shared/cli
+    ./shared/tools
   ];
 }
