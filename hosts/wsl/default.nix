cfca6e879328f5331e3dbcaa9ac802312fab6c8e
Pham Duc Binh <binh1298@gmail.com>
2024-07-31 11:38:31 +0700

    add wsl config

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,4 +1,16 @@
 {pkgs, ...}: {
+  users = {
+    users = {
+      BinhWSL = {
+        isNormalUser = true;
+        description = "binh1298";
+        initialPassword = "123123";
+        shell = pkgs.zsh;
+        extraGroups = ["networkmanager" "wheel" "input" "docker" "libvirtd"];
+      };
+    };
+  };
+
   programs.zsh.enable = true;
   environment = {
     shells = with pkgs; [bash zsh];
