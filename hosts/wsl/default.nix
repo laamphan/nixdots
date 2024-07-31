8745dee8fdd346b84853916bc3fb1c06119fb7fa
Pham Duc Binh <binh1298@gmail.com>
2024-07-31 11:32:57 +0700

    add wsl config

---
+++ hosts/wsl/default.nix
@@ -0,0 +1,15 @@
+{pkgs, ...}: {
+  programs.zsh.enable = true;
+  environment = {
+    shells = with pkgs; [bash zsh];
+    loginShell = pkgs.zsh;
+    systemPackages = [pkgs.coreutils];
+  };
+  nix.extraOptions = ''
+    experimental-features = nix-command flakes
+  '';
+
+  services.nix-daemon.enable = true;
+
+  users.users.binh-wsl.home = "/Users/binh-wsl";
+}
