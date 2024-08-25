f269320bb1e5bcc45b60b0e707268e176610d65a
laamphan <41721718+laamphan@users.noreply.github.com>
2024-08-25 15:59:00 +0700

    minor changes

--- home/pc/cli/nixvim/conform.nix
+++ home/pc/cli/nixvim/conform.nix
@@ -8,37 +8,39 @@
       #   timeoutMs = 2500;
       #   lspFallback = true;
       # };
-      formattersByFt = {
-        lua = ["stylua"];
-        # Conform will run multiple formatters sequentially
-        python = ["isort" "black"];
-        # Use a sub-list to run only the first available formatter
-        javascript = [["biome-check" "prettier"]];
-        typescript = [["biome-check" "prettier"]];
-        tsx = ["biome-check" "prettier" "rustywind"];
-        jsx = ["biome-check" "prettier" "rustywind"];
-        typescriptreact = ["biome-check" "prettier" "rustywind"];
-        javascriptreact = ["biome-check" "prettier" "rustywind"];
-        nix = [["alejandra"]];
-        go = ["gofmt" "gofumt" "goimports_reviser" "golines"];
-        rust = [["rustfmt"]];
-        # yaml = [["prettier"]];
-        json = [["biome-check" "prettier"]];
-        sql = [["sqlfmt"]];
-        html = [["biome-check" "prettier"]];
-        css = [["biome-check" "prettier"]];
-        markdown = [["prettier"]];
-        graphql = [["biome-check" "prettier"]];
+      settings = {
+        formatters_by_ft = {
+          lua = ["stylua"];
+          # Conform will run multiple formatters sequentially
+          python = ["isort" "black"];
+          # Use a sub-list to run only the first available formatter
+          javascript = [["biome-check" "prettier"]];
+          typescript = [["biome-check" "prettier"]];
+          tsx = ["biome-check" "prettier" "rustywind"];
+          jsx = ["biome-check" "prettier" "rustywind"];
+          typescriptreact = ["biome-check" "prettier" "rustywind"];
+          javascriptreact = ["biome-check" "prettier" "rustywind"];
+          nix = [["alejandra"]];
+          go = ["gofmt" "gofumt" "goimports_reviser" "golines"];
+          rust = [["rustfmt"]];
+          # yaml = [["prettier"]];
+          json = [["biome-check" "prettier"]];
+          sql = [["sqlfmt"]];
+          html = [["biome-check" "prettier"]];
+          css = [["biome-check" "prettier"]];
+          markdown = [["prettier"]];
+          graphql = [["biome-check" "prettier"]];
 
-        # Use the "*" filetype to run formatters on all filetypes.
-        # "*" = ["codespell"];
-        # Use the "_" filetype to run formatters on filetypes that don't
-        # have other formatters configured.
-        "_" = ["trim_whitespace"];
-      };
-      formatters = {
-        "biome-check" = {
-          require_cwd = true;
+          # Use the "*" filetype to run formatters on all filetypes.
+          # "*" = ["codespell"];
+          # Use the "_" filetype to run formatters on filetypes that don't
+          # have other formatters configured.
+          "_" = ["trim_whitespace"];
+        };
+        formatters = {
+          "biome-check" = {
+            require_cwd = true;
+          };
         };
       };
     };
