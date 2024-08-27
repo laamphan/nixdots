b76b987006aa528485e70c150d4bf85279935c55
laamphan <41721718+laamphan@users.noreply.github.com>
2024-08-27 21:09:59 +0700

    fix: correct user's path

--- packages/nvchad/custom/configs/lspconfig.lua
+++ packages/nvchad/custom/configs/lspconfig.lua
@@ -49,7 +49,7 @@ lspconfig.lua_ls.setup({
 })
 
 lspconfig.apex_ls.setup({
-	apex_jar_path = "/home/binh1298/.local/share/nvim/mason/packages/apex-language-server/extension/dist/apex-jorje-lsp.jar",
+	apex_jar_path = "/home/w/.local/share/nvim/mason/packages/apex-language-server/extension/dist/apex-jorje-lsp.jar",
 	capabilities = capabilities,
 	on_attach = on_attach,
 	filetypes = { "apexcode", "apex", "visualforce" },
